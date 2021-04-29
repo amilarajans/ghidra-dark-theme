8ceb1e210859eb8191109e6fec875828dffe4b0c
Amila Kumara <amilarajans@gmail.com>
2021-04-29 21:53:30 +0530

    added few options

--- src/main/java/darktheme/DarkThemePlugin.java
+++ src/main/java/darktheme/DarkThemePlugin.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,18 +15,16 @@
  */
 package darktheme;
 
-import static ghidra.docking.util.DockingWindowsLookAndFeelUtils.LAST_LOOK_AND_FEEL_KEY;
-import static ghidra.docking.util.DockingWindowsLookAndFeelUtils.getInstalledLookAndFeelName;
-import static ghidra.docking.util.DockingWindowsLookAndFeelUtils.getLookAndFeelNames;
+import java.awt.Window;
 
-import java.util.List;
-
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLaf;
 
-import docking.options.editor.StringWithChoicesEditor;
 import docking.tool.ToolConstants;
 import ghidra.app.plugin.PluginCategoryNames;
 import ghidra.framework.main.FrontEndOnly;
@@ -45,94 +43,152 @@ import ghidra.util.SystemUtilities;
 
 //@formatter:off
 @PluginInfo(
-	status = PluginStatus.STABLE,
-	packageName = DarkThemePluginPackage.NAME,
-	category = PluginCategoryNames.SUPPORT,
-	shortDescription = "Dark theme",
-	description = "This plugin provides Dark theme to Ghidra"
+        status = PluginStatus.STABLE,
+        packageName = DarkThemePluginPackage.NAME,
+        category = PluginCategoryNames.SUPPORT,
+        shortDescription = "Dark theme",
+        description = "This plugin provides Dark theme to Ghidra"
 )
 //@formatter:on
 public class DarkThemePlugin extends Plugin implements FrontEndOnly, OptionsChangeListener {
 
-	private String selectedLookAndFeel;
-	public final static String LOOK_AND_FEEL_NAME = "Swing Look And Feel";
-	private final static String OPTIONS_TITLE = ToolConstants.TOOL_OPTIONS;
-	private final static String DARK = "Dark";
-	private final static String DARK_LAF = "com.formdev.flatlaf.FlatDarculaLaf";
+    // setting names
+    private final static String DARK_THEME = "Dark theme";
+    private final static String OPTIONS_TITLE = "Theme";
+    private final static String MENU_BAR_EMBEDDED = "Menu Bar Embedded";
+    private final static String UNIFIED_TITLE_BAR = "Unified Title Bar";
 
-	private static boolean issuedLafNotification;
+    // preference names
+    private final static String IS_DARK_THEME_ENABLED = "IsDarkThemeEnabled";
+    private final static String IS_MENU_BAR_EMBEDDED_ENABLED = "IsMenuBarEmbeddedEnabled";
+    private final static String IS_UNIFIED_TITLE_BAR_ENABLED = "IsUnifiedTitleBarEnabled";
 
-	public DarkThemePlugin(PluginTool tool) {
-		super(tool);
+    private boolean issuedLafNotification;
 
-		SystemUtilities.assertTrue(tool instanceof FrontEndTool, "Plugin added to the wrong type of tool");
-		initLookAndFeelOptions();
-	}
+    private boolean isDarkThemeEnabled;
+    private boolean isMenuBarEmbeddedEnabled;
+    private boolean isUnifiedTitleBarEnabled;
 
-	private void initLookAndFeelOptions() {
+    public DarkThemePlugin(PluginTool tool) {
+        super(tool);
 
-		ToolOptions opt = tool.getOptions(OPTIONS_TITLE);
+        SystemUtilities.assertTrue(tool instanceof FrontEndTool, "Plugin added to the wrong type of tool");
+        initLookAndFeelOptions();
+    }
 
-		selectedLookAndFeel = getInstalledLookAndFeelName();
-		List<String> lookAndFeelNames = getLookAndFeelNames();
-		lookAndFeelNames.add(DARK);
-		opt.registerOption(LOOK_AND_FEEL_NAME, OptionType.STRING_TYPE, selectedLookAndFeel,
-				new HelpLocation(ToolConstants.TOOL_HELP_TOPIC, "Look_And_Feel"),
-				"Set the look and feel for Ghidra.  After you change the "
-						+ "look and feel, you will have to restart Ghidra to see the effect.",
-				new StringWithChoicesEditor(lookAndFeelNames));
-		selectedLookAndFeel = opt.getString(LOOK_AND_FEEL_NAME, selectedLookAndFeel);
+    private static boolean isDisplayableFrameOrDialog(Window w) {
+        return w.isDisplayable() && (w instanceof JFrame || w instanceof JDialog);
+    }
 
-		opt.addOptionsChangeListener(this);
-	}
+    private void initLookAndFeelOptions() {
 
-	@Override
-	public void optionsChanged(ToolOptions options, String optionName, Object oldValue, Object newValue) {
+        ToolOptions opt = tool.getOptions(OPTIONS_TITLE);
 
-		if (optionName.equals(LOOK_AND_FEEL_NAME)) {
-			String newLookAndFeel = (String) newValue;
-			if (!newLookAndFeel.equals(selectedLookAndFeel)) {
-				issueLaFNotification();
-			}
+        isDarkThemeEnabled = Boolean.parseBoolean(Preferences.getProperty(IS_DARK_THEME_ENABLED, Boolean.FALSE.toString()));
+        opt.registerOption(DARK_THEME, OptionType.BOOLEAN_TYPE, isDarkThemeEnabled,
+                new HelpLocation(ToolConstants.TOOL_HELP_TOPIC, "Dark_theme"),
+                "Set the dark theme for Ghidra");
+        isDarkThemeEnabled = opt.getBoolean(DARK_THEME, isDarkThemeEnabled);
 
-			saveLookAndFeel((String) newValue);
-		}
-	}
+        isUnifiedTitleBarEnabled = Boolean.parseBoolean(Preferences.getProperty(IS_UNIFIED_TITLE_BAR_ENABLED, Boolean.FALSE.toString()));
+        opt.registerOption(UNIFIED_TITLE_BAR, OptionType.BOOLEAN_TYPE, isUnifiedTitleBarEnabled,
+                new HelpLocation(ToolConstants.TOOL_HELP_TOPIC, "Unified_Background"),
+                "Set the unified title bar for Ghidra. (Enable Dark theme)");
 
-	private void saveLookAndFeel(String lookAndFeelName) {
-		selectedLookAndFeel = lookAndFeelName;
-		Preferences.setProperty(LAST_LOOK_AND_FEEL_KEY, selectedLookAndFeel);
-		Preferences.store();
-	}
+        isUnifiedTitleBarEnabled = opt.getBoolean(UNIFIED_TITLE_BAR, isUnifiedTitleBarEnabled);
 
-	private void issueLaFNotification() {
-		if (issuedLafNotification) {
-			return;
-		}
+        isMenuBarEmbeddedEnabled = Boolean.parseBoolean(Preferences.getProperty(IS_MENU_BAR_EMBEDDED_ENABLED, Boolean.FALSE.toString()));
+        opt.registerOption(MENU_BAR_EMBEDDED, OptionType.BOOLEAN_TYPE, isMenuBarEmbeddedEnabled,
+                new HelpLocation(ToolConstants.TOOL_HELP_TOPIC, "Menu_Bar_Embedded"),
+                "Embed the menu bar in title bar for Ghidra. (Enable Dark theme)");
 
-		issuedLafNotification = true;
-		Msg.showInfo(getClass(), null, "Look And Feel Updated",
-				"The new Look and Feel will take effect \nafter you exit and restart Ghidra.");
-	}
+        isMenuBarEmbeddedEnabled = opt.getBoolean(MENU_BAR_EMBEDDED, isMenuBarEmbeddedEnabled);
 
-	@Override
-	protected void init() {
-		super.init();
-		if (Preferences.getProperty(LAST_LOOK_AND_FEEL_KEY).equals(DARK)) {
-			try {
-				FlatLightLaf.install();
-				FlatDraculaIJTheme.install();
-				UIManager.setLookAndFeel(DARK_LAF);
-			} catch (Exception exc) {
-				Msg.error(DarkThemePlugin.class, "Error loading Look and Feel: " + exc, exc);
-			}
-		}
-	}
+        opt.addOptionsChangeListener(this);
+    }
 
-	@Override
-	public void dispose() {
-		ToolOptions opt = tool.getOptions(OPTIONS_TITLE);
-		opt.removeOptionsChangeListener(this);
-		super.dispose();
-	}
+    @Override
+    public void optionsChanged(ToolOptions options, String optionName, Object oldValue, Object newValue) {
+        // since all the values are booleans (for now)
+        boolean newBooleanValue = Boolean.parseBoolean(newValue.toString());
+        String value = Boolean.toString(newBooleanValue);
+        if (optionName.equals(DARK_THEME)) {
+            if (newBooleanValue) {
+                issueLaFNotification();
+            }
+            Preferences.setProperty(IS_DARK_THEME_ENABLED, value);
+            Preferences.store();
+        }
+
+        if (optionName.equals(UNIFIED_TITLE_BAR)) {
+            if (newBooleanValue) {
+                issueLaFNotification();
+            }
+            Preferences.setProperty(IS_UNIFIED_TITLE_BAR_ENABLED, value);
+            Preferences.store();
+        }
+
+        if (optionName.equals(MENU_BAR_EMBEDDED)) {
+            if (newBooleanValue) {
+                issueLaFNotification();
+            }
+            Preferences.setProperty(IS_MENU_BAR_EMBEDDED_ENABLED, value);
+            Preferences.store();
+        }
+    }
+
+    private void issueLaFNotification() {
+        if (issuedLafNotification) {
+            return;
+        }
+
+        issuedLafNotification = true;
+        Msg.showInfo(getClass(), null, "Settings changed",
+                "The new changes will take effect \nafter you exit and restart Ghidra.");
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        if (isDarkThemeEnabled) {
+            FlatDarculaLaf.install();
+            updateUI();
+        }
+
+        // set the window decoration mode
+        FlatLaf.setUseNativeWindowDecorations(isUnifiedTitleBarEnabled || isMenuBarEmbeddedEnabled);
+
+        updateEmbeddedMenuBar();
+
+        updateTitleBar();
+    }
+
+    private void updateTitleBar() {
+        UIManager.put("TitlePane.unifiedBackground", isUnifiedTitleBarEnabled);
+        SystemUtilities.runSwingLater(FlatLaf::repaintAllFramesAndDialogs);
+    }
+
+    private void updateEmbeddedMenuBar() {
+        UIManager.put("TitlePane.menuBarEmbedded", isMenuBarEmbeddedEnabled);
+        SystemUtilities.runSwingLater(FlatLaf::revalidateAndRepaintAllFramesAndDialogs);
+    }
+
+    private void updateUI() {
+        SystemUtilities.runSwingLater(() -> {
+            // iterate through all the windows
+            for (Window w : Window.getWindows()) {
+                if (isDisplayableFrameOrDialog(w)) {
+                    // update UI components
+                    SwingUtilities.updateComponentTreeUI(w);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void dispose() {
+        ToolOptions opt = tool.getOptions(OPTIONS_TITLE);
+        opt.removeOptionsChangeListener(this);
+        super.dispose();
+    }
 }
--- src/main/java/darktheme/DarkThemePluginPackage.java
+++ src/main/java/darktheme/DarkThemePluginPackage.java
@@ -7,7 +7,6 @@ public class DarkThemePluginPackage extends PluginPackage {
 	public static final String NAME = "DarkTheme";
 
 	public DarkThemePluginPackage() {
-		super(NAME, ResourceManager.loadImage("images/dracula.svg"),
-				"These plugins are just examples of how to write a plugin.", CORE_PRIORITY);
+		super(NAME, ResourceManager.loadImage("images/dracula.svg"), "Dark theme plugin", CORE_PRIORITY);
 	}
 }
